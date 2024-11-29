18eb56e4036cfc7a599a98a95078c43e577c5df7
lymane <lymane0121@gmail.com>
2024-11-29 16:48:38 +0800

    locale: 翻译

--- plugins/nexus-blobstore-s3/src/main/resources/static/rapture/NX/s3blobstore/app/PluginStrings.js
+++ plugins/nexus-blobstore-s3/src/main/resources/static/rapture/NX/s3blobstore/app/PluginStrings.js
@@ -15,45 +15,54 @@
 /**
  * @since 3.17
  */
-Ext.define('NX.s3blobstore.app.PluginStrings', {
-  '@aggregate_priority': 90,
+Ext.define(
+  "NX.s3blobstore.app.PluginStrings",
+  {
+    "@aggregate_priority": 90,
 
-  singleton: true,
-  requires: [
-    'NX.I18n'
-  ],
+    singleton: true,
+    requires: ["NX.I18n"],
 
-  keys: {
-    S3Blobstore_Help: '<em>S3 blob stores require specific permissions to support full provisioning and functionality through Nexus Repository Manager. ' +
-        'Consult our <a href="https://links.sonatype.com/products/nexus/blobstores/s3/docs" target="_blank">documentation</a>' +
-        ' for the specific set of permissions required.</em>',
-    S3Blobstore_Region_FieldLabel: 'Region',
-    S3Blobstore_Region_HelpText: 'The AWS Region to use',
-    S3Blobstore_Bucket_FieldLabel: 'Bucket',
-    S3Blobstore_Bucket_HelpText: 'S3 Bucket Name (must be between 3 and 63 characters long containing only lower-case characters, numbers, periods, and dashes)',
-    S3Blobstore_Prefix_FieldLabel: 'Prefix',
-    S3Blobstore_Prefix_HelpText: 'S3 Path prefix',
-    S3Blobstore_Expiration_FieldLabel: 'Expiration Days',
-    S3Blobstore_Expiration_HelpText: 'How many days until deleted blobs are finally removed from the S3 bucket (-1 to disable)',
-    S3Blobstore_Authentication_Title: 'Authentication',
-    S3Blobstore_Authentication_AccessKeyId: 'Access Key ID',
-    S3Blobstore_Authentication_SecretAccessKey: 'Secret Access Key',
-    S3Blobstore_Authentication_AssumeRoleArn: 'Assume Role ARN (Optional)',
-    S3Blobstore_Authentication_SessionToken: 'Session Token ARN (Optional)',
-    S3Blobstore_EncryptionSettings_Title: 'Encryption',
-    S3Blobstore_EncryptionSettings_Type_FieldLabel: 'Encryption Type',
-    S3Blobstore_EncryptionSettings_Type_HelpText: 'The type of encryption for objects in the S3 Blob Store',
-    S3Blobstore_EncryptionSettings_KeyID_FieldLabel: 'KMS Key ID (Optional)',
-    S3Blobstore_EncryptionSettings_KeyID_HelpText: 'If using KMS encryption, you can supply a Key ID. If left blank, then the default will be used',
-    S3Blobstore_AdvancedConnectionSettings_Title: 'Advanced Connection Settings',
-    S3Blobstore_AdvancedConnectionSettings_EndPointUrl_FieldLabel: 'Endpoint URL',
-    S3Blobstore_AdvancedConnectionSettings_EndPointUrl_HelpText: 'A custom endpoint URL for third party object stores using the S3 API',
-    S3Blobstore_AdvancedConnectionSettings_SignatureVersion_FieldLabel: 'Signature Version',
-    S3Blobstore_AdvancedConnectionSettings_SignatureVersion_HelpText: 'An API signature version which may be required for third party object stores using the S3 API',
-    S3Blobstore_AdvancedConnectionSettings_PathStyleAccess_FieldLabel: 'Use path-style access',
-    S3Blobstore_AdvancedConnectionSettings_PathStyleAccess_HelpText: 'Setting this flag will result in path-style access being used for all requests'
+    keys: {
+      S3Blobstore_Help:
+        '<em>S3 Blob 存储需要特定的权限才能通过 Nexus Repository Manager 完成完整的配置和功能支持。<a href="https://links.sonatype.com/products/nexus/blobstores/s3/docs" target="_blank">请参阅我们的文档</a>了解所需的特定权限。</em>',
+      S3Blobstore_Region_FieldLabel: "区域",
+      S3Blobstore_Region_HelpText: "要使用的 AWS 区域",
+      S3Blobstore_Bucket_FieldLabel: "存储桶",
+      S3Blobstore_Bucket_HelpText:
+        "S3 存储桶名称（必须介于 3 到 63 个字符之间，仅包含小写字母、数字、句点和连字符）",
+      S3Blobstore_Prefix_FieldLabel: "前缀",
+      S3Blobstore_Prefix_HelpText: "S3 路径前缀",
+      S3Blobstore_Expiration_FieldLabel: "过期天数",
+      S3Blobstore_Expiration_HelpText:
+        "已删除的 blob 从 S3 存储桶中完全删除之前的天数（-1 表示禁用）",
+      S3Blobstore_Authentication_Title: "身份验证",
+      S3Blobstore_Authentication_AccessKeyId: "访问密钥 ID",
+      S3Blobstore_Authentication_SecretAccessKey: "秘密访问密钥",
+      S3Blobstore_Authentication_AssumeRoleArn: "假设角色 ARN（可选）",
+      S3Blobstore_Authentication_SessionToken: "会话令牌 ARN（可选）",
+      S3Blobstore_EncryptionSettings_Title: "加密",
+      S3Blobstore_EncryptionSettings_Type_FieldLabel: "加密类型",
+      S3Blobstore_EncryptionSettings_Type_HelpText:
+        "S3 Blob 存储中对象的加密类型",
+      S3Blobstore_EncryptionSettings_KeyID_FieldLabel: "KMS 密钥 ID（可选）",
+      S3Blobstore_EncryptionSettings_KeyID_HelpText:
+        "如果使用 KMS 加密，可以提供密钥 ID。如果留空，将使用默认值",
+      S3Blobstore_AdvancedConnectionSettings_Title: "高级连接设置",
+      S3Blobstore_AdvancedConnectionSettings_EndPointUrl_FieldLabel: "端点 URL",
+      S3Blobstore_AdvancedConnectionSettings_EndPointUrl_HelpText:
+        "用于第三方对象存储的 S3 API 的自定义端点 URL",
+      S3Blobstore_AdvancedConnectionSettings_SignatureVersion_FieldLabel:
+        "签名版本",
+      S3Blobstore_AdvancedConnectionSettings_SignatureVersion_HelpText:
+        "第三方对象存储可能要求的 API 签名版本",
+      S3Blobstore_AdvancedConnectionSettings_PathStyleAccess_FieldLabel:
+        "使用路径样式访问",
+      S3Blobstore_AdvancedConnectionSettings_PathStyleAccess_HelpText:
+        "设置此标志将使所有请求使用路径样式访问",
+    },
+  },
+  function (obj) {
+    NX.I18n.register(obj);
   }
-
-}, function(obj) {
-  NX.I18n.register(obj);
-});
+);
--- plugins/nexus-example-content/src/main/resources/static/rapture/NX/example/app/PluginStrings.js
+++ plugins/nexus-example-content/src/main/resources/static/rapture/NX/example/app/PluginStrings.js
@@ -15,17 +15,19 @@
 /**
  * Example content plugin strings.
  */
-Ext.define('NX.example.app.PluginStrings', {
-  '@aggregate_priority': 90,
+Ext.define(
+  "NX.example.app.PluginStrings",
+  {
+    "@aggregate_priority": 90,
 
-  singleton: true,
-  requires: [
-    'NX.I18n'
-  ],
+    singleton: true,
+    requires: ["NX.I18n"],
 
-  keys: {
-    Repository_Facet_ExampleFacet_Title: 'Example Settings',
+    keys: {
+      Repository_Facet_ExampleFacet_Title: "示例配置",
+    },
+  },
+  function (self) {
+    NX.I18n.register(self);
   }
-}, function(self) {
-  NX.I18n.register(self);
-});
+);
